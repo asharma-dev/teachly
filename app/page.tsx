72be15b1040eebbafe9cfbc5762d3597237c0c59
Abhishek <56558684+abhisheksharma1123@users.noreply.github.com>
2024-04-18 19:08:44 -0700

    cleanup

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
-
-import {TopNavigation} from "@/app/ui/header/Nav"
+import { TopNavigation } from "@/app/ui/header/Nav";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-      <TopNavigation />
-       <p>abhishek</p>
-       </div>
+      <div className="z-10 w-full max-w-5xl items-center font-mono text-sm ">
+        <TopNavigation />
+      </div>
     </main>
   );
 }
---
+++ app/ui/header/Nav.tsx
@@ -0,0 +1,9 @@
+export const TopNavigation = () => {
+  return (
+    <div className="nav-container">
+      <div className="logo-brand">Logo</div>
+
+      <div className="nav-links">Links</div>
+    </div>
+  );
+};
